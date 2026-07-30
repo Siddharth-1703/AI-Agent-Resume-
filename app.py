a174ef916a8504fc4ef475bea0d068b6cede03a8
Siddharth-1703 <khuranasiddharth35@gmail.com>
2026-07-30 11:09:02 +0530

    Refactor app.py to improve structure and clarity

--- app.py
+++ app.py
@@ -155,6 +155,6 @@ if st.button ("Generate Resume"):
                   st.html(code , width="Stretch",
                   unsafe_allow_javascript=True)
                   st.divider() #to give horizontal div
-                   job_code=get_jobs(agent,location,profile)
+                  job_code=get_jobs(agent,location,profile)
                   jobs_code=get_jobs(agent,location,profile)
                   st.html(job_code,width="stretch",unsafe_allow_javascript=True)

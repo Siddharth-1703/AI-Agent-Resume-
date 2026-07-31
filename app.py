5635f820f7a6315c2d383d8ef8f65dba9e62a718
Siddharth-1703 <khuranasiddharth35@gmail.com>
2026-07-31 09:55:27 +0530

    Refactor app.py for improved functionality

--- app.py
+++ app.py
@@ -153,7 +153,7 @@ Show atleast Top 10-20 results with direct apply link"""
 if st.button ("Generate Resume"):
          with st.spinner("Agent Running"):
                   code=main_agent(agent,user_info)
-                  st.html(code , width="Stretch",
+                  st.html(code , width="stretch",
                   unsafe_allow_javascript=True)
                   st.divider() #to give horizontal div
                   job_code=get_jobs(agent,location,profile)
